273e480618bb16d56c9b3c3dfb83a65b80ed0e32
Beanway144 <65977044+Beanway144@users.noreply.github.com>
2023-12-10 00:03:22 -0500

    [cbenway] added benchmarks (#58)

---
+++ benchmarks/dead.lisp
@@ -0,0 +1,1 @@
+(print (let ((a 1)) (let ((b 2)) (let ((c 3))  (let ((d 4)) (let ((e 5)) (let ((f 6)) (let ((g (+ a (+ b (+ c (+ d (+ e f))))))) 99))))))))
---
+++ benchmarks/if-prop.lisp
@@ -0,0 +1,2 @@
+(define (f a b) (if (+ 9 8) a b))
+(print (if (f true false) (if false 3 4) (if 1 2 9)))
